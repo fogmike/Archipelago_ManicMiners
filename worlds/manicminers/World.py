c62bfaa42d3cbbc10231a96457f24343c639dff7
fogmike <michael_arden@btinternet.com>
2026-03-23 14:32:34 +0000

    Fix capitalisation on World.py

--- worlds/manicminers/World.py
+++ worlds/manicminers/World.py
@@ -22,19 +22,19 @@ class ManicMinersWorld(World):
     origin_region_name = "Menu"
     
     def create_regions(self) -> None:
-        regions.create_and_connect_regions(self)
-        locations.create_all_locations(self)
+        Regions.create_and_connect_regions(self)
+        Locations.create_all_locations(self)
     
     def set_rules(self) -> None:
-        rules.set_all_rules(self)
+        Rules.set_all_rules(self)
         
     def create_items(self) -> None:
-        items.create_all_items(self)
+        Items.create_all_items(self)
         
     def create_item(self, name: str) -> Items.ManicMinersItem:
         return Items.create_item_with_correct_classification(self, name)
 
     def get_filler_item_name(self) -> str:
-        return items.get_random_filler_item_name(self)
+        return Items.get_random_filler_item_name(self)
     
     #TODO: Do I need fill_slot_data?
